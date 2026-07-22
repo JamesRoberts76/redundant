ef1141760ec527f30cbd2c26b487459f29032614
JamesRoberts76 <james.progressivepersonnel@gmail.com>
2026-07-22 18:43:33 +0100

    Create network-config.js

---
+++ network-config.js
@@ -0,0 +1,8 @@
+name = "compressed-master-fascia"
+compatibility_date = "2026-07-22"
+
+[site]
+bucket = "./public"
+
+[vars]
+# OPENAI_API_KEY should be set securely via `wrangler secret put OPENAI_API_KEY`
